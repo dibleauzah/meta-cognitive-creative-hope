718093088b7799cf8239e8b7fdbee7be2c906e11
Seif Sekalala <seifsekalala@gmail.com>
2026-05-22 07:52:46 -0400

    Fix syntax error in print statement

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 #Adding code soon. 
 
-print("Why hello-there, worldies! :-) So nice of y'all to visit our humble online abode!");
+print[("Why hello-there, worldies! :-) So nice of y'all to visit our humble online abode!")];
